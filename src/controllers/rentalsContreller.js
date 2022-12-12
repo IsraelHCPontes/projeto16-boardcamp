fe9495e2d92f550241a415e908dbc75f287837ed
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-12 14:27:15 -0300

    feat(CRUD/rentals)Implementei a funcionalidade de listar, inserir, atualizar e deletar

--- src/controllers/rentalsContreller.js
+++ src/controllers/rentalsContreller.js
@@ -1,4 +1,5 @@
 import db from '../database/db.js';
+import dayjs from 'dayjs';
 
 export async function getRentals(req, res){
     const customerId = req.query.customerId;
@@ -20,13 +21,16 @@ export async function getRentals(req, res){
              [customerId, gameId]);
 
             const rentalsRes = rentals.rows.map(rental =>{
+
+                let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
+
                  return{
                     id: rental.id ,
                     customerId: rental.customerId,
                     gameId: rental.gameId,
-                    rentDate: rental.rentDate,
+                    rentDate: rentalsFormated,
                     daysRented: rental.daysRented,
-                    returnDate: rental.returnDate, 
+                    returnDate: rental.returnDate,
                     originalPrice: rental.originalPrice,
                     delayFee: rental.delayFee,
                     customer: {
@@ -46,17 +50,51 @@ export async function getRentals(req, res){
             return;
         }else{
             const rentals = await db.query(`
-            SELECT * 
-            FROM rentals`);
-            res.send(rentals.rows);
+             SELECT rentals.*, customers.name AS "customersName" , games.name AS "gameName", games."categoryId" AS "gamecategoryId", categories.name AS "categoryName"
+             FROM rentals
+             JOIN customers
+             ON rentals."customerId" = customers.id
+             JOIN games 
+             ON rentals."gameId" =  games.id
+             JOIN categories 
+             ON games."categoryId" =  categories.id`)
+
+             const rentalsRes = rentals.rows.map(rental =>{
+
+                let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
+                let returnDateFormated = dayjs(rental.returnDate).format('YYYY-MM-DD')
+
+                 return{
+                    id: rental.id ,
+                    customerId: rental.customerId,
+                    gameId: rental.gameId,
+                    rentDate: rentalsFormated,
+                    daysRented: rental.daysRented,
+                    returnDate: rental.returnDate === null? null : returnDateFormated,
+                    originalPrice: rental.originalPrice,
+                    delayFee: rental.delayFee,
+                    customer: {
+                     id: rental.customerId,
+                     name: rental.customersName
+                    },
+                    game: {
+                      id: rental.gameId,
+                      name: rental.gameName,
+                      categoryId: rental.gamecategoryId,
+                      categoryName: rental.categoryName
+                    }
+                  }
+                })
+
+            res.send(rentalsRes);
             return;
-        } 
+         
+     }
     }catch(error){
         console.log(error)
         res.sendStatus(500)
     }
 }
-
 
 export async function postRentals(req, res){
     const{
@@ -105,28 +143,72 @@ export async function postRentals(req, res){
     }
 }
 
-export async function putCustomers(req, res){
+export async function postRentalsReturn(req, res){
     const id = req.params.id;
-    const {
-        name,
-        phone,
-        cpf,
-        birthday
-    } = req.body;
+    const today = dayjs().format('YYYY-MM-DD')
+    let delayFree = null
 
     try{
+        const rental = await db.query(`
+        SELECT *
+        FROM rentals
+        WHERE id = $1`,
+        [id])
+
+        if(!rental){
+            res.sendStatus(404)
+            return;
+        }
+
+        if(rental.rows[0].returnDate !== null){
+            res.sendStatus(400)
+            return;
+        }
+
+        let rentDateFormated = dayjs(rental.rows[0].rentDate).format('YYYY-MM-DD')
+        let lastDay = dayjs().diff(rentDateFormated, 'day')
+        if(lastDay > rental.daysRented){
+           delayFree = rental.originalPrice * lastDay
+        }
         await db.query(`
-        UPDATE customers
-        SET  name=$1, phone=$2, cpf=$3, birthday=$4
-        WHERE id=$5 
-        `, [name,
-            phone,
-            cpf,
-            birthday,
-            id])
+        UPDATE rentals
+        SET "returnDate"=$1, "delayFee"=$2
+        WHERE id=$3
+        `, [today,delayFree,id])
         res.sendStatus(201)    
     }catch(error){
         console.log(error)
         res.sendStatus(500)
     }
 }
+
+export async function deleteRentals(req, res){
+    const id = req.params.id;
+
+    try{
+        const rental = await db.query(`
+        SELECT *
+        FROM rentals
+        WHERE id = $1`,
+        [id])
+
+
+        if(!rental){
+            res.sendStatus(404)
+            return;
+        }
+
+        if(rental.rows[0].returnDate !== null){
+            res.sendStatus(400)
+            return;
+        }
+
+        await db.query(`DELETE FROM rentals WHERE id=$1`, [id])
+
+        res.sendStatus(200);
+    }catch(error){
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
+
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getRentals, postRentals } from "../controllers/rentalsContreller.js";
+import { getRentals, postRentals,postRentalsReturn, deleteRentals } from "../controllers/rentalsContreller.js";
 import rentalsValidation from "../middlewares/rentalsMidlleware .js";
 
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.get("/rentals", getRentals);
 router.post("/rentals", rentalsValidation, postRentals);
-
+router.post("/rentals/:id/return", postRentalsReturn)
+router.delete("/rentals/:id", deleteRentals);
 export default router;
