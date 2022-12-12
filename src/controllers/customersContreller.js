ca165036526599f76e82c87deec893b7c8bb72e6
israelpontes <israelpontes@MacBook-Pro-de-israel.local>
2022-12-12 15:47:07 -0300

    fix()

--- src/controllers/customersContreller.js
+++ src/controllers/customersContreller.js
@@ -1,4 +1,5 @@
 import db from '../database/db.js';
+import dayjs from 'dayjs';
 
 export async function getCustomers(req, res){
     const cpf = req.query.cpf;
@@ -9,13 +10,34 @@ export async function getCustomers(req, res){
              FROM  customers
              WHERE cpf 
              LIKE $1`, [`%${cpf}%`]);
-            res.send(customers.rows);
+             const cutomersPackage = customers.rows.map(customer =>{
+                customer.birthday = dayjs(customer.birthday).format('YYYY-MM-DD')
+            })
+            res.send(cutomersPackage);
             return;
         }else{
             const customers = await db.query(`
             SELECT * 
             FROM customers`);
-            res.send(customers.rows);
+            const cutomersPackage = customers.rows.map(({ 
+                id,
+                name,
+                phone,
+                cpf,
+                birthday}) =>{
+                const birthdayFormated = dayjs(birthday).format('YYYY-MM-DD')
+                 return(
+                    {
+                        id,
+                        name,
+                        phone,
+                        cpf,
+                        birthday: birthdayFormated
+                    }
+                 )   
+            })
+            
+            res.send(cutomersPackage);
             return;
         } 
     }catch(error){
@@ -32,7 +54,25 @@ export async function getCustomersId(req, res){
         FROM  customers
         WHERE customers.id = $1`,
         [id]);
-        res.send(customers.rows);
+
+        const cutomersPackage = customers.rows.map(({ 
+            id,
+            name,
+            phone,
+            cpf,
+            birthday}) =>{
+            const birthdayFormated = dayjs(birthday).format('YYYY-MM-DD')
+             return(
+                {
+                    id,
+                    name,
+                    phone,
+                    cpf,
+                    birthday: birthdayFormated
+                }
+             )   
+        })
+        res.send(cutomersPackage);
     }catch(error){
         console.log(error)
         res.sendStatus(404)
--- src/controllers/rentalsContreller.js
+++ src/controllers/rentalsContreller.js
@@ -4,9 +4,11 @@ import dayjs from 'dayjs';
 export async function getRentals(req, res){
     const customerId = req.query.customerId;
     const gameId = req.query.gameId;
+    let rentalsRes;
 
+    
     try{
-        if(customerId || gameId){
+        if(customerId && !gameId){
             const rentals = await db.query(`
              SELECT rentals.*, customers.name AS "customersName" , games.name AS "gameName", games."categoryId" AS "gamecategoryId", categories.name AS "categoryName"
              FROM rentals
@@ -16,11 +18,10 @@ export async function getRentals(req, res){
              ON rentals."gameId" =  games.id
              JOIN categories 
              ON games."categoryId" =  categories.id
-             WHERE rentals."customerId" = $1
-             OR rentals."gameId" = $2`,  
-             [customerId, gameId]);
+             WHERE rentals."customerId" = $1`, 
+             [customerId]);
 
-            const rentalsRes = rentals.rows.map(rental =>{
+             rentalsRes = rentals.rows.map(rental =>{
 
                 let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
 
@@ -44,11 +45,100 @@ export async function getRentals(req, res){
                       categoryName: rental.categoryName
                     }
                   }
-            })     
+                  
+            }) 
+            res.send(rentalsRes);
+            return;  
+        }  
 
+
+            if(!customerId &&  gameId){
+                const rentals = await db.query(`
+                 SELECT rentals.*, customers.name AS "customersName" , games.name AS "gameName", games."categoryId" AS "gamecategoryId", categories.name AS "categoryName"
+                 FROM rentals
+                 JOIN customers
+                 ON rentals."customerId" = customers.id
+                 JOIN games 
+                 ON rentals."gameId" =  games.id
+                 JOIN categories 
+                 ON games."categoryId" =  categories.id
+                 WHERE rentals."gameId" = $1`,  
+                 [gameId]);
+    
+                 rentalsRes = rentals.rows.map(rental =>{
+    
+                    let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
+    
+                     return{
+                        id: rental.id ,
+                        customerId: rental.customerId,
+                        gameId: rental.gameId,
+                        rentDate: rentalsFormated,
+                        daysRented: rental.daysRented,
+                        returnDate: rental.returnDate,
+                        originalPrice: rental.originalPrice,
+                        delayFee: rental.delayFee,
+                        customer: {
+                         id: rental.customerId,
+                         name: rental.customersName
+                        },
+                        game: {
+                          id: rental.gameId,
+                          name: rental.gameName,
+                          categoryId: rental.gamecategoryId,
+                          categoryName: rental.categoryName
+                        }
+                      } 
+                })  
+                res.send(rentalsRes);
+                return;  
+            } 
+
+                if(customerId &&  gameId){
+                    const rentals = await db.query(`
+                     SELECT rentals.*, customers.name AS "customersName" , games.name AS "gameName", games."categoryId" AS "gamecategoryId", categories.name AS "categoryName"
+                     FROM rentals
+                     JOIN customers
+                     ON rentals."customerId" = customers.id
+                     JOIN games 
+                     ON rentals."gameId" =  games.id
+                     JOIN categories 
+                     ON games."categoryId" =  categories.id
+                     WHERE rentals."gameId" = $1
+                     AND rentals."costumerId" = $2`, 
+                     [gameId, customerId]);
+        
+                     rentalsRes = rentals.rows.map(rental =>{
+        
+                        let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
+        
+                         return{
+                            id: rental.id ,
+                            customerId: rental.customerId,
+                            gameId: rental.gameId,
+                            rentDate: rentalsFormated,
+                            daysRented: rental.daysRented,
+                            returnDate: rental.returnDate,
+                            originalPrice: rental.originalPrice,
+                            delayFee: rental.delayFee,
+                            customer: {
+                             id: rental.customerId,
+                             name: rental.customersName
+                            },
+                            game: {
+                              id: rental.gameId,
+                              name: rental.gameName,
+                              categoryId: rental.gamecategoryId,
+                              categoryName: rental.categoryName
+                            }
+                          }
+                    }) 
             res.send(rentalsRes);
             return;
-        }else{
+        }
+
+        console.log(gameId, customerId)
+        if(!customerId && !gameId){
             const rentals = await db.query(`
              SELECT rentals.*, customers.name AS "customersName" , games.name AS "gameName", games."categoryId" AS "gamecategoryId", categories.name AS "categoryName"
              FROM rentals
@@ -59,7 +149,7 @@ export async function getRentals(req, res){
              JOIN categories 
              ON games."categoryId" =  categories.id`)
 
-             const rentalsRes = rentals.rows.map(rental =>{
+              rentalsRes = rentals.rows.map(rental =>{
 
                 let rentalsFormated = dayjs(rental.rentDate).format('YYYY-MM-DD')
                 let returnDateFormated = dayjs(rental.returnDate).format('YYYY-MM-DD')
@@ -85,11 +175,8 @@ export async function getRentals(req, res){
                     }
                   }
                 })
-
-            res.send(rentalsRes);
-            return;
-         
-     }
+        }
+        res.send(rentalsRes);
     }catch(error){
         console.log(error)
         res.sendStatus(500)
